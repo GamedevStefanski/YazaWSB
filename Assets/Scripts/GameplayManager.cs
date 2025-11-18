ca98b4e42803c8d704e9b58e8ecac8066afba0ea
Przemysław Stefański <gamedev.stefanski@gmail.com>
2025-11-19 00:15:16 +0100

    Fixed a combat bug, implemented a modular shop system

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,14 +4,30 @@ public class GameplayManager : MonoBehaviour
 {
     public int ink;
     [SerializeField] private GameObject InkTextObject;
-    void Start()
+
+    public int selectedChampionID = 0;
+    [SerializeField] private GameObject[] availableShops;
+    [SerializeField] private GameObject shopParentObject;
+
+    void Awake()
     {
+        // Ignore collision between allied mobs
         Physics2D.IgnoreLayerCollision(6, 6);
         Physics2D.IgnoreLayerCollision(7, 7);
+
+        ShopSetup();
     }
 
     private void Update()
     {
+        // Update ink value visible on the screen
         InkTextObject.GetComponent<TMPro.TextMeshProUGUI>().text = "Ink owned: " + ink;
     }
+
+    public void ShopSetup()
+    {
+        GameObject currentShop = Instantiate(availableShops[selectedChampionID], shopParentObject.transform);
+        currentShop.transform.SetParent(shopParentObject.transform);
+    }
+
 }
