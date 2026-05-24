1e12eb030f0ebf11a8b17ac9cc1b5077b4abe685
Przemysław Stefański <gamedev.stefanski@gmail.com>
2026-05-24 13:58:42 +0200

    Background + Camera fixes

--- Assets/Scripts/cameramovement/skryptkamerki.cs
+++ Assets/Scripts/cameramovement/skryptkamerki.cs
@@ -4,73 +4,83 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Camera cam; // obiekt kamery
 
     [SerializeField]
-    private Camera cam; //obiekt kamery
+    private float zoomStep, minCamSize, maxCamSize; // zakres powiêkszenia
 
+    [Header("Ustawienia granic mapy")]
     [SerializeField]
-    private float zoomStep, minCamSize, maxCamSize; //zakres powiêkszenia
+    private Transform mapRoot;
 
-    [SerializeField]
-    private SpriteRenderer mapRenderer;
     private float mapMinX, mapMaxX, mapMinY, mapMaxY;
-
     private Vector3 dragOrigin;
-
 
     private void Awake()
     {
-        mapMinX = mapRenderer.transform.position.x - mapRenderer.bounds.size.x / 2f;
-        mapMaxX = mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2f;
-
-        mapMinY = mapRenderer.transform.position.y - mapRenderer.bounds.size.y / 2f;
-        mapMaxY = mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2f;
+        CalculateMapBounds();
     }
-
 
     void Start()
     {
-
+        cam.transform.position = ClampCamera(cam.transform.position);
     }
 
     void Update()
     {
         PanCamera();
+        HandleZoom(); // Dodano obs³ugê kó³ka myszy
     }
 
-    //przesuwanie pozycji kamery (lewo, prawo, góra, dó³)
+    private void CalculateMapBounds()
+    {
+        if (mapRoot == null) return;
+
+        SpriteRenderer[] renderers = mapRoot.GetComponentsInChildren<SpriteRenderer>();
+        if (renderers.Length == 0) return;
+
+        Bounds combinedBounds = renderers[0].bounds;
+        foreach (SpriteRenderer sr in renderers)
+        {
+            combinedBounds.Encapsulate(sr.bounds);
+        }
+
+        mapMinX = combinedBounds.min.x;
+        mapMaxX = combinedBounds.max.x;
+        mapMinY = combinedBounds.min.y;
+        mapMaxY = combinedBounds.max.y;
+    }
+
     private void PanCamera()
     {
-
         if (Input.GetMouseButtonDown(0))
             dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButton(0))
         {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-            print("origin " + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference" + difference);
-            
-
-            cam.transform.position = ClampCamera(cam.transform.position + difference); 
-
+            cam.transform.position = ClampCamera(cam.transform.position + difference);
         }
-
     }
 
-    public void ZoomIn()
+    // Nowa funkcja obs³uguj¹ca scroll myszki
+    private void HandleZoom()
     {
-        float newSize = cam.orthographicSize - zoomStep;
-        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
-        
-        cam.transform.position = ClampCamera(cam.transform.position);  
-    }
+        // Pobiera wartoœæ ze scrolla (zazwyczaj jest to u³amek np. 0.1 lub -0.1 za jedno 'tykniêcie')
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
 
-    public void ZoomOut()
-    {
-        float newSize = cam.orthographicSize + zoomStep;
-        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+        if (scrollData != 0f)
+        {
+            // Mno¿ymy przez zoomStep (i dodatkowo x10, poniewa¿ scrollData to bardzo ma³e wartoœci)
+            float newSize = cam.orthographicSize - (scrollData * zoomStep * 10f);
 
-        cam.transform.position = ClampCamera(cam.transform.position);
+            // Zaciskamy w dozwolonych granicach
+            cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+
+            // Poprawiamy pozycjê, ¿eby kamera nie wysz³a poza mapê przy oddalaniu
+            cam.transform.position = ClampCamera(cam.transform.position);
+        }
     }
 
     private Vector3 ClampCamera(Vector3 targetPosition)
